1ef22129a7423c520ee2bf0707589eeb41a5c910
Sam Sonter <samuel.aarga@gmail.com>
2021-12-15 06:48:33 +0800

    template inheritance

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ def home():
 
 @app.route('/about/')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title= "About")
 
 @app.route('/contact/')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', title="contact")
 
 
 @app.route('/portfolio/')
 def portfolio():
-    return render_template('portfolio.html')
+    return render_template('portfolio.html', title="portfolio")
 
 
 if __name__ == '__main__':
